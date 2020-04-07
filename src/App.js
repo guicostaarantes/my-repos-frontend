0eb5372984c13104f610237a52d50c0b297318d3
guicostaarantes <arantes.g@outlook.com>
2020-04-06 23:00:15 -0300

    enhanced style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import api from "./services/api";
+import { Input, Button, Chip } from "@material-ui/core";
+import ChipInput from "material-ui-chip-input";
 
 import "./styles.css";
 
 function App() {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
-  const [techs, setTechs] = useState("");
+  const [techs, setTechs] = useState([]);
   const [repos, setRepos] = useState([]);
 
   const getRepos = async () => {
@@ -21,12 +23,12 @@ function App() {
   async function handleAddRepository(event) {
     event.preventDefault();
     try {
-      const response = await api.post("repositories", {
-        title,
-        url,
-        techs: techs.split(","),
-      });
+      const response = await api.post("repositories", { title, url, techs });
+      console.log(response.data);
       setRepos([...repos, response.data]);
+      setTitle("");
+      setUrl("");
+      setTechs([]);
     } catch (err) {
       console.log(err);
     }
@@ -43,37 +45,66 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        {repos.map((repo, index) => (
-          <li key={index}>
-            {repo.title}
-            <button onClick={() => handleRemoveRepository(repo.id, index)}>
-              Remover
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div>
+    <div className="container">
+      <div className="repos-container">
+        <h2>Meus repositórios</h2>
+        {repos.length === 0 && (
+          <div className="no-repo-message">
+            <p>Não há repositórios cadastrados</p>
+          </div>
+        )}
+        <ul data-testid="repository-list">
+          {repos.map((repo, index) => (
+            <li key={index}>
+              <div className="repo-data-container">
+                <h4>{repo.title}</h4>
+                <a href={repo.url}>{repo.url}</a>
+                <div className="chip-container">
+                  {repo.techs.map((tech, ind) => (
+                    <Chip key={ind} size="small" label={tech} />
+                  ))}
+                </div>
+              </div>
+              <div className="button-container">
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => handleRemoveRepository(repo.id, index)}
+                >
+                  Remover
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="add-repo-container">
         <h2>Adicionar repositório</h2>
-        <form onSubmit={handleAddRepository}>
-          <input
+        <div className="add-repo-form">
+          <Input
             placeholder="Nome do repositório"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <input
+          <Input
             placeholder="URL do repositório"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
-          <input
-            placeholder="Tecnologias do repositório (separados por vírgula)"
+          <ChipInput
+            placeholder="Tecnologias"
             value={techs}
-            onChange={(e) => setTechs(e.target.value)}
+            blurBehavior="add"
+            onAdd={(chip) => setTechs([...techs, chip])}
+            onDelete={(_chip, index) =>
+              setTechs(techs.filter((t, i) => i !== index))
+            }
+            newChipKeyCodes={[9, 13, 186, 188]}
           />
-          <button type="submit">Adicionar</button>
-        </form>
+          <Button variant="contained" onClick={handleAddRepository}>
+            Adicionar
+          </Button>
+        </div>
       </div>
     </div>
   );
